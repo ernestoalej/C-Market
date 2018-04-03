6954f784bf11d5d023accf653a60b49e10c89a83
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-04-03 15:22:16 -0400

    Part 18 Agregar Producto Min 6:06

--- C-Market/Controllers/OrdersController.cs
+++ C-Market/Controllers/OrdersController.cs
@@ -1,9 +1,7 @@
 ﻿using C_Market.Models;
 using C_Market.ViewModels;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace C_Market.Controllers
@@ -19,9 +17,11 @@ namespace C_Market.Controllers
             orderView.Customer = new Customer();
             orderView.Products = new List<ProductOrder>();
 
+            Session["orderView"] = orderView;
+
             var list = db.Customers.ToList();
 
-            list.Add(new Customer { CustomerID = 0, FullName = "[Seleccione un cliente]" });
+            list.Add(new Customer { CustomerID = 0, FirstName = "[You must select a customer]" });
             list = list.OrderBy(c => c.FullName).ToList();
             ViewBag.CustomerID = new SelectList(list, "CustomerID", "FullName");
             
@@ -32,19 +32,40 @@ namespace C_Market.Controllers
         public ActionResult NewOrder(OrderView orderView)
         {                 
             var list = db.Customers.ToList();
-
-            list.Add(new Customer { CustomerID = 0, FullName = "[Seleccione un cliente]" });
+           
+            list.Add(new Customer { CustomerID = 0, FirstName = "[You must select a customer]" });
             list = list.OrderBy(c => c.FullName).ToList();
             ViewBag.CustomerID = new SelectList(list, "CustomerID", "FullName");
 
             return View(orderView);
         }
 
-        public ActionResult AddProduct(ProductOrder productOrder)
+        public ActionResult AddProduct()
         {
             var list = db.Products.ToList();
-            list = list.OrderBy(c => c.Description).ToList();            
+            list.Add(new ProductOrder { ProductID = 0, Description = "[You must select a product]" });
+            list = list.OrderBy(p => p.Description).ToList();              
             ViewBag.ProductID = new SelectList(list, "ProductID", "Description");
+                   
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(ProductOrder productOrder)
+        {
+            var orderView = Session["orderView"] as OrderView;
+
+            var productID = int.Parse(Request["ProductID"] );
+
+            if (productID == 0)
+            {
+                var list = db.Products.ToList();
+                list.Add(new ProductOrder { ProductID = 0, Description = "[You must select a product]" });
+                list = list.OrderBy(p => p.Description).ToList();
+                ViewBag.ProductID = new SelectList(list, "ProductID", "Description");
+
+                return View(productOrder);
+            }
 
             return View(productOrder);
         }
